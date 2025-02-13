8e7fcdb39cadc41636406d8fa561a2cb7d2f06d0
Ryan Gabriel <ryangabrielsilvag052@gmail.com>
2025-02-13 15:45:55 -0300

    Delete Ativ_4 directory

--- Ativ_4/Result.java
+++
@@ -1,9 +0,0 @@
-package Ativ_4;
-
-public class Result {
-
-    public static String verificar(double nota1, double nota2) {
-        double media = (nota1 + nota2) / 2;
-        return (media >= 7) ? "O aluno foi aprovado" : "O aluno NÃO foi aprovado";
-    }
-}
